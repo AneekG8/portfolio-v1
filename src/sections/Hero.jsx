4647fc694c8195a4714682517d8dccd4e70e6d2c
Aneek Ghosh <aneek.ghosh@rebelfoods.com>
2025-04-19 05:13:32 +0530

    Hero 3D model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,31 +1,42 @@
 import { Button } from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants";
 
 const Hero = () => {
   return (
-   <section id="hero" >
-    <header className="md:w-full w-screen md:px-20 px-5">
-        <div className="flex flex-col gap-7">
-            <div className="hero-text">
-                <h1>Shaping 
-                    <span className="slide">
-                        <span className="wrapper">
-                            {words.map((word) => (
-                                <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-                                    <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
-                                    <span>{word.text}</span>
-                                </span>
-                            ))}
+   <section id="hero" className="relative overflow-hidden">
+     <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="" />
+      </div>
+    <div className="hero-layout">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5 z-10 pointer-events-none">
+            <div className="flex flex-col gap-7">
+                <div className="hero-text">
+                    <h1>Shaping 
+                        <span className="slide">
+                            <span className="wrapper">
+                                {words.map((word) => (
+                                    <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                        <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                                        <span>{word.text}</span>
+                                    </span>
+                                ))}
+                            </span>
                         </span>
-                    </span>
-                </h1>
-                <h1>into Real Projects</h1>
-                <h1>that Deliver Results</h1>
+                    </h1>
+                    <h1>into Real Projects</h1>
+                    <h1>that Deliver Results</h1>
+                </div>
+                <p className="text-white-50 md:text-md text-sm relative z-10 pointer-events-none">Hi, I am Aneek. A fullstack web developer with 3+ years of professional experience.</p>
+                <Button className="md:w-80 md:h-16 w-60 h-12" text="See My Work" id="seeMyWorkBtn"/>
             </div>
-            <p className="text-white-50 md:text-xl text-sm">Hi, I am Aneek. A fullstack web developer with 3+ years of professional experience.</p>
-            <Button className="md:w-80 md:h-16 w-60 h-12" text="See My Work" id="seeMyWorkBtn"/>
-        </div>
-    </header>
+        </header>
+        <figure>
+            <div className="hero-3d-layout">
+                <HeroExperience/>
+            </div>
+        </figure>
+    </div>
    </section>
   )
 }
