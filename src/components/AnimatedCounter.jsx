42f63fef525236a3237bf62a711aefece0596b8c
Aneek Ghosh <aneek.ghosh@rebelfoods.com>
2025-04-19 16:05:29 +0530

    minor fix for hero model position

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -6,11 +6,11 @@ const AnimatedCounter = () => {
     <div id="counter" className="padding-x-lg xl:mt-0 mt-12">
         <div className="mx-auto grid-4-cols">
             {counterItems.map((item,index) => (
-                <div key={index} className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
-                    <div className="counter-number text-5xl text-white font-bold mb-2">
+                <div key={index} className="bg-zinc-900 rounded-lg md:p-10 p-5 flex flex-col justify-center">
+                    <div className="counter-number md:text-5xl text-3xl text-white font-bold mb-2">
                        <CountUp scrollSpyOnce enableScrollSpy end={item.value} suffix={item.suffix} />
                     </div>
-                    <div className="text-white-50 text-lg">
+                    <div className="text-white-50 md:text-lg text-sm">
                         {item.label}
                     </div>
                 </div>
