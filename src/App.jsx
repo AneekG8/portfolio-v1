dcdb95fa91ee8684b33c54c9773b0193fed08f8b
Aneek Ghosh <aneek.ghosh@rebelfoods.com>
2025-04-19 20:33:03 +0530

    feature cards section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from './sections/Hero'
 import ShowCase from './sections/ShowCase'
 import Navbar from './components/Navbar'
+import FeatureCards from './sections/FeatureCards'
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <ShowCase />
+      <FeatureCards />
     </>
   )
 }
