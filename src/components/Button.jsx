87166f731e96c9b17cd3b4b5753de194021fd97f
Aneek Ghosh <aneek.ghosh@rebelfoods.com>
2025-04-26 21:23:57 +0530

    Integrated emailjs and form functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,26 @@
 import React from "react"
 
-export const Button = ({text,className,component = 'button',href,iconPath,...props}) => {
+export const Button = ({text, className, component = 'button', href, iconPath, loading = false, ...props}) => {
     return (
         React.createElement(component, {
             className: `${className} cta-wrapper`,
             href,
+            disabled: loading,
             children: (
-                <div className="cta-button group">
+                <div className="cta-button group relative">
                     <div className="bg-circle"/>
-                    <p className="text">
-                        {text}
-                    </p>
-                    <div className="arrow-wrapper">
-                        <img src={iconPath} alt="arrow" />
-                    </div>
+                    {loading ? (
+                        <div className="w-6 h-6 border-4 border-white/20 border-t-white/90 rounded-full animate-spin"/>
+                    ) : (
+                        <>
+                            <p className="text">
+                                {text}
+                            </p>
+                            <div className="arrow-wrapper">
+                                <img src={iconPath} alt="arrow" />
+                            </div>
+                        </>
+                    )}
                 </div>
             ),
             ...props
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,8 +1,125 @@
 import SectionTitle from "../components/SectionTitle"
 import { Button } from "../components/Button"
 import ComputerModelExperience from "../components/models/ComputerModels/ComputerModelExperience"
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+    const formRef = useRef(null);
+    const formFields = [
+        {
+            name: "name",
+            label: "Name",
+            component: "input",
+            componentProps: {
+                type: "text",
+                placeholder: "Enter your name",
+            },
+        },
+        {
+            name: "email",
+            label: "Email",
+            component: "input",
+            componentProps: {
+                type: "email",
+                placeholder: "Enter your email",
+            },
+            validate: (value) => {
+                const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                if (!emailRegex.test(value)) {
+                    return "Please enter a valid email address";
+                }
+            }
+        },
+        {
+            name: "message",
+            label: "Message",
+            component: "textarea",
+            componentProps: {
+                placeholder: "Enter your message",
+                rows: 6,
+            },
+        }
+    ]
+    const [loading, setLoading] = useState(false);
+    const emptyFormData = {
+        name: {
+            value: "",
+            error: "",
+        },
+        email: {
+            value: "",
+            error: "",
+        },
+        message: {
+            value: "",
+            error: "",
+        },
+    }
+    const [formData, setFormData] = useState(emptyFormData)
+    const [error, setError] = useState("");
+    
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: {
+                value,
+                error: "",
+            },
+        }));
+    };
+
+    const validateForm = ()=>{
+        let isFormValid = true;
+        formFields.forEach(({name, validate}) => {
+            if(!formData[name].value) {
+                isFormValid = false;
+                setFormData(prev => ({
+                    ...prev,
+                    [name]: {
+                        ...prev[name],
+                        error: "This field is required",
+                    },
+                }));
+                return;
+            }
+            if(validate) {
+                const error = validate(formData[name].value);
+                if(error) {
+                    isFormValid = false;
+                    setFormData(prev => ({
+                        ...prev,
+                        [name]: {
+                            ...prev[name],
+                            error,
+                        },
+                    }));
+                }
+            }
+        })
+        return isFormValid;
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setLoading(true);
+        setError("");
+        if(!validateForm()) {
+            setLoading(false);
+            return;
+        }
+        try {
+            await emailjs.sendForm(import.meta.env.VITE_EMAIL_JS_SERVICE_ID, import.meta.env.VITE_EMAIL_JS_TEMPLATE_ID, formRef.current, import.meta.env.VITE_EMAIL_JS_PUBLIC_KEY);
+            setFormData(emptyFormData);
+        } catch (error) {
+            console.log('Failed to send email!', error);
+            setError("Something went wrong! Please try again");
+        }finally{
+            setLoading(false);
+        }
+    };
+
   return (
     <section id="contact" className="mt-32 section-padding flex-center">
         <div className="w-full h-full md:px-10 px-5">
@@ -10,25 +127,27 @@ const Contact = () => {
             <div className="mt-16 grid grid-cols-1 lg:grid-cols-12 gap-10">
                 <div className="lg:col-span-6">
                     <div className="card-border rounded-xl md:p-10 p-5 flex-center">
-                        <form onSubmit={(e)=>{e.preventDefault()}} className="w-full flex flex-col flex-center gap-7">
-                            <div className="w-full">
-                                <label htmlFor="name">Name</label>
-                                <input type="text" name="name" id="name" placeholder="Enter your name"/>
-                            </div>
-                            <div className="w-full">
-                                <label htmlFor="email">Email</label>
-                                <input type="email" name="email" id="email" placeholder="Enter your email"/>
-                            </div>
-                            <div className="w-full">
-                                <label htmlFor="message">Message</label>
-                                <textarea name="message" id="message" placeholder="Enter your message" rows="6"></textarea>
-                            </div>
+                        <form noValidate ref={formRef} onSubmit={handleSubmit} className="w-full flex flex-col flex-center gap-7">
+                            {formFields.map((field) => (
+                                <div key={field.name} className="w-full">
+                                    <label htmlFor={field.name}>{field.label}</label>
+                                    <field.component
+                                        {...field.componentProps}
+                                        name={field.name}
+                                        value={formData[field.name].value}
+                                        onChange={handleInputChange}
+                                    />
+                                    {formData[field.name].error && <p className="text-red-400">{formData[field.name].error}</p>}
+                                </div>
+                            ))}
                             <Button
                                 iconPath="/images/arrow-right.svg"
                                 className="md:h-16 w-full h-12 send-button" 
-                                text="Send Message" 
+                                text={loading ? "Sending..." : "Send Message"}
                                 type="submit"
+                                loading={loading}
                             />
+                            {error && <p className="text-red-500">{error}</p>}
                         </form>
                     </div>
                 </div>
