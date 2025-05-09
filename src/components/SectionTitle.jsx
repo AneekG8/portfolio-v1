d8ef0b862d00a8f84a1ef27db5f00d892521de57
Aneek Ghosh <aneek.ghosh@rebelfoods.com>
2025-05-09 21:03:23 +0530

    hover on tabs

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -5,7 +5,7 @@ const SectionTitle = ({title,subtitle,tabs,selectedTab,onTabChange}) => {
     <div className='flex flex-col items-center gap-5'>
         <div className="flex rounded-full border-black-200 border-1">
             {tabs ? tabs.map((tab,index) => (
-                <div key={index} className={`hero-badge cursor-pointer ${index !== selectedTab ? 'bg-black' : ''}`} onClick={() => onTabChange(index)}>
+                <div key={index} className={`hero-badge cursor-pointer ${index !== selectedTab ? 'bg-black hover:bg-black-100' : ''}`} onClick={() => onTabChange(index)}>
                     <p>{tab.subtitle}</p>
                 </div>
             )) : <div className="hero-badge">
